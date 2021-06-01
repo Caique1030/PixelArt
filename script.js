a673016968f3aba0233eb8f17c942e0b163aa5bc
Rafael Romano <rafa-kun@hotmail.com>
2021-06-01 20:26:42 -0300

    Adicionando exercicios ate o 8 graças a deus

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ paletas[0].style.backgroundColor = 'black';
 paletas[1].style.backgroundColor = 'blue';
 paletas[2].style.backgroundColor = 'green';
 paletas[3].style.backgroundColor = 'pink';
+
+
+let colors=document.querySelector('#color-palette');
+
+
+colors.addEventListener('click',function(event){
+    const colorSelected=document.querySelector('.selected');
+    colorSelected.classList.remove('selected');
+    event.target.classList.add('selected');
+})
+  
