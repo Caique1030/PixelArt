7fda120da42894b75604cab96b0e4c1074e51d5a
Rafael Romano <rafa-kun@hotmail.com>
2021-06-01 16:37:35 -0300

    tentando o 2

---
+++ script.js
@@ -0,0 +1,5 @@
+let paletas=document.querySelectorAll('.color')
+paletas[0].style.backgroundColor="red"
+paletas[1].style.backgroundColor="blue"
+paletas[2].style.backgroundColor="green"
+paletas[3].style.backgroundColor="pink"
