29818c434a96edbab1cd445204c7d4fb75687742
Rafael Romano <rafa-kun@hotmail.com>
2021-06-01 21:18:51 -0300

    adicionando exercicios ate o 8 e arrumando o lint

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
-let paletas = document.querySelectorAll('.color');
+const paletas = document.querySelectorAll('.color');
 paletas[0].style.backgroundColor = 'black';
 paletas[1].style.backgroundColor = 'blue';
 paletas[2].style.backgroundColor = 'green';
 paletas[3].style.backgroundColor = 'pink';
 
+const colors = document.querySelector('#color-palette');
 
-let colors=document.querySelector('#color-palette');
+colors.addEventListener('click', (event) => {
+  const colorSelected = document.querySelector('.selected');
+  colorSelected.classList.remove('selected');
+  event.target.classList.add('selected');
+});
 
+const divPixelBoard = document.querySelector('#pixel-board');
+let colorSelected = 'rgb(0, 0, 0)';
+colors.addEventListener('click', () => {
+  const selectedColor = document.querySelector('.selected');
+  colorSelected = getComputedStyle(selectedColor).backgroundColor;
+  return colorSelected;
+});
 
-colors.addEventListener('click',function(event){
-    const colorSelected=document.querySelector('.selected');
-    colorSelected.classList.remove('selected');
-    event.target.classList.add('selected');
-})
-  
+divPixelBoard.addEventListener('click', (click) => {
+  const clickMouse = click;
+  clickMouse.target.style.backgroundColor = colorSelected;
+});
